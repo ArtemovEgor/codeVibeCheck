5465845e474604b5c91ecce0ce975fa2ddbbf4a2
Ekateryna Volkova <volkova5005@gmail.com>
2026-03-22 21:40:47 +0200

    feat: add loadTopics, loadProgress

--- src/pages/library/library.ts
+++ src/pages/library/library.ts
@@ -1,14 +1,44 @@
 import BaseComponent from "@/components/base/base-component";
 import type Page from "../page";
+import type { ITopic } from "@/types/shared/widget.types";
+import type { IUserTopicProgress } from "@/types/shared/user.types";
+import { widgetsApi } from "@/api/widgets.api";
+import { progressApi } from "@/api/progress.api";
 
 export class Library extends BaseComponent implements Page {
+  private topics: ITopic[] = [];
+  private progress: IUserTopicProgress[] = [];
+
   constructor() {
     super({ tag: "div", className: "library" });
 
     this.init();
   }
 
-  public init(): void {
-    this.addChildren([]);
+  public async init(): Promise<void> {
+    [this.topics, this.progress] = await Promise.all([
+      this.loadTopics(),
+      this.loadProgress(),
+    ]);
+
+    console.log(this.topics);
+    console.log(this.progress);
+  }
+
+  private async loadTopics(): Promise<ITopic[]> {
+    try {
+      const { data } = await widgetsApi.getTopics();
+      return data;
+    } catch {
+      return [];
+    }
+  }
+
+  private async loadProgress(): Promise<IUserTopicProgress[]> {
+    try {
+      return await progressApi.getAll();
+    } catch {
+      return [];
+    }
   }
 }
