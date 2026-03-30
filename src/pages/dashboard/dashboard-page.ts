40a929e7a7a00f9b5064f764402987626caf5e76
Ekateryna Volkova <volkova5005@gmail.com>
2026-03-30 21:27:25 +0300

    feat: add dashboard layout

--- src/pages/dashboard/dashboard-page.ts
+++ src/pages/dashboard/dashboard-page.ts
@@ -1,14 +1,44 @@
 import BaseComponent from "@/components/base/base-component";
 import type Page from "../page";
+import "./dashboard-page.scss";
 
 export class DashboardPage extends BaseComponent implements Page {
+  private interactSector: BaseComponent | undefined = undefined;
+  private learningSector: BaseComponent | undefined = undefined;
+  private header: BaseComponent | undefined = undefined;
+
   constructor() {
     super({ tag: "div", className: "dashboard" });
-
     this.init();
   }
 
   public init(): void {
-    this.addChildren([]);
+    this.getNode().replaceChildren();
+    this.renderMainLayout();
+  }
+
+  private renderMainLayout() {
+    this.header = new BaseComponent({
+      className: "dashboard__header",
+      parent: this,
+    });
+
+    const divider = new BaseComponent({
+      className: "dashboard__divider",
+    });
+
+    this.learningSector = new BaseComponent({
+      className: "dashboard__learning-sector",
+    });
+
+    this.interactSector = new BaseComponent({
+      className: "dashboard__interact-sector",
+    });
+
+    new BaseComponent({
+      className: "dashboard__columns",
+      parent: this,
+      children: [this.learningSector, divider, this.interactSector],
+    });
   }
 }
