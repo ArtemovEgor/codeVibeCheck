d66e8ae200cebef7591e3f18d1752a2c73e809de
Egor Artemov <mercury2juice@gmail.com>
2026-06-11 16:29:10 +0400

    test: add langing page tests

---
+++ tests/globals.d.ts
@@ -0,0 +1,7 @@
+/* eslint-disable unicorn/require-module-specifiers */
+
+declare global {
+  var __coverage__: unknown;
+}
+
+export {};
--- tests/pages/landing-page.ts
+++ tests/pages/landing-page.ts
@@ -1,4 +1,5 @@
-import { expect, Locator, Page } from "@playwright/test";
+import { expect } from "@playwright/test";
+import type { Locator, Page } from "@playwright/test";
 
 export class LandingPage {
   readonly page: Page;
